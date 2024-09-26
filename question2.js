9e50d6da58c484f1a3ef4d966bdb4fcc82617414
kiannadalton <kiannadalton@aol.com>
2024-09-26 18:33:52 -0400

    completed question2

--- question2.js
+++ question2.js
@@ -1,8 +1,8 @@
 // Question 2
-// Reverse the given singly linked list by manipulating its head and return the reversed list. The input is head = [1,2,3,4,5] and the expected output is [5,4,3,2,1]. 
-
+// Reverse the given singly linked list by manipulating its head and return the reversed list. The input is head = [1,2,3,4,5] and the expected output is [5,4,3,2,1].
 
 class ListNodeVal {
+  // creates list
   constructor(val = 0, next = null) {
     this.val = val;
     this.next = next;
@@ -10,6 +10,7 @@ class ListNodeVal {
 }
 
 function reverseGivenList(head, prev = null) {
+  // base case, stops when head equals null
   if (head === null) {
     return prev;
   }
@@ -25,16 +26,16 @@ head.next.next = new ListNodeVal(3);
 head.next.next.next = new ListNodeVal(4);
 head.next.next.next.next = new ListNodeVal(5);
 
-// Reverse the linked list
+// Reverse the linked list using the recursion
 const reversedList = reverseGivenList(head);
 
 // Print the reversed list values
-let currVal = reversedList;
+let currentVal = reversedList;
 const result = [];
 
-while (currVal !== null) {
-  result.push(currVal.val);
-  currVal = currVal.next;
+while (currentVal !== null) {
+  result.push(currentVal.val);
+  currentVal = currentVal.next;
 }
 
 console.log(result); // Output: [5, 4, 3, 2, 1]
